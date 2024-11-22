bf66cde37567e707a41a3f787144b588650af4cb
Federico Cavedoni <fedecave2000@gmail.com>
2024-11-22 17:21:29 +0100

    First mockup

---
+++ EdgeComputing_Project/out/clang-debug/src/BaseStation.o.d
@@ -0,0 +1,6 @@
+../out/clang-debug/src/BaseStation.o: BaseStation.cc BaseStation.h \
+  C:/Users/fedec/Desktop/omnetpp-6.0.3/include/omnetpp.h
+
+BaseStation.h:
+
+C:/Users/fedec/Desktop/omnetpp-6.0.3/include/omnetpp.h:
---
+++ EdgeComputing_Project/out/clang-debug/src/User.o.d
@@ -0,0 +1,8 @@
+../out/clang-debug/src/User.o: User.cc User.h \
+  C:/Users/fedec/Desktop/omnetpp-6.0.3/include/omnetpp.h BaseStation.h
+
+User.h:
+
+C:/Users/fedec/Desktop/omnetpp-6.0.3/include/omnetpp.h:
+
+BaseStation.h:
